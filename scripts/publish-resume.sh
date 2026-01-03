b02baa4bd032d85c506cce513a7ad80e0959a7e1
ankit <sahuankit.murhu@gmail.com>
2026-01-04 00:26:19 +0530

    refactor: summary added and ats enhancing

--- scripts/publish-resume.sh
+++ scripts/publish-resume.sh
@@ -16,6 +16,6 @@ echo -e "\e[33mPushing to GitHub...\e[0m"
 echo -e "\e[33mPushing to GitHub...\e[0m"
 git add -- webresume.pdf
 git commit -m "fix: update web resume"
-git push origin main
+git push origin nofetch/web
 
 echo -e "\e[32mResume published successfully.\e[0m"
